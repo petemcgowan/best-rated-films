fd8b1475cf4bee875e68169356ced3a2f0707ed9
Pete <peteburquette@gmail.com>
2021-06-18 13:02:14 +0100

    merging offline mobx / movie styling work

--- client/src/utils/helpers.js
+++ client/src/utils/helpers.js
@@ -1,18 +1,28 @@
-import store from '../store';
+import store from "../store";
+
+export const randomNumber = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
 
 export function getEmail() {
-  const localEmail = localStorage.getItem('BRFemail');
-  console.log ("getEmail, localEmail" +  JSON.stringify(localEmail));
+  const localEmail = localStorage.getItem("BRFemail");
+  console.log("getEmail, localEmail" + JSON.stringify(localEmail));
   const state = store.getState();
   var email = "";
-  if ((state.auth !== undefined) && (state.auth !==null) && (state.auth.user !==null))  {
-    console.log ("getEmail, state.user.email" +  JSON.stringify(state.auth.user.email));
+  if (
+    state.auth !== undefined &&
+    state.auth !== null &&
+    state.auth.user !== null
+  ) {
+    console.log(
+      "getEmail, state.user.email" + JSON.stringify(state.auth.user.email)
+    );
     email = state.auth.user.email;
+  } else if (localEmail !== null && localEmail !== "") {
+    console.log("getEmail, setting email to local storage version");
+    email = "Jun15@gmail.com";
+    // HACK Pete Todo
   }
-  else if ((localEmail !== null) && (localEmail !== "")) {
-    console.log ("getEmail, setting email to local storage version");
-    email = localEmail;
-  }
-  console.log ("getEmail, state" +  JSON.stringify(state));
+  console.log("getEmail, state" + JSON.stringify(state));
   return email;
 }
