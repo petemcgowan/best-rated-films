ebc5377702fe5b56ea1b9cb3ac383c608492dc5e
Pete <peteburquette@gmail.com>
2021-08-18 18:58:05 +0100

    vintage mode nav adds

--- client/src/components/AppNavbar.js
+++ client/src/components/AppNavbar.js
@@ -9,115 +9,113 @@ import {
   Container,
 } from "reactstrap";
 import { connect } from "react-redux";
-import { Switch, Route } from "react-router-dom";
+import homeStore from "../store/HomeStore";
 import PropTypes from "prop-types";
 import RegisterModal from "./auth/RegisterModal";
 import LoginModal from "./auth/LoginModal";
 import Logout from "./auth/Logout";
 import logo from "../images/best rated_logo_rgb_dark_white.png";
+import { WatchedLink } from "../components/WatchedLink";
+import VintageLink from "./VintageLink";
+import { observer } from "mobx-react";
 
-import { GlobalProvider } from "../context/GlobalState";
-import { FilmProvider } from "../context/FilmState";
-import { WatchedProvider } from "../context/WatchedState";
-import { FilmList } from "./FilmList";
-import { WatchedList } from "./WatchedList";
-// import { FilmContext } from "../context/FilmState";
+const AppNavbar = observer(
+  class extends Component {
+    state = {
+      isOpen: false,
+      // isVintage: true,
+    };
 
-class AppNavbar extends Component {
-  state = {
-    isOpen: false,
-  };
+    static propTypes = {
+      auth: PropTypes.object.isRequired,
+    };
 
-  static propTypes = {
-    auth: PropTypes.object.isRequired,
-  };
+    toggle = () => {
+      this.setState({
+        isOpen: !this.state.isOpen,
+      });
+    };
 
-  toggle = () => {
-    this.setState({
-      isOpen: !this.state.isOpen,
-    });
-  };
+    toggleVintage = () => {
+      const { changePage, currentPage } = this.props;
+      console.log(
+        "toggleVintage Before, homeStore.vintageMode:" + homeStore.vintageMode
+      );
+      // this.setState({
+      //   isVintage: !this.state.isVintage,
+      // });
+      homeStore.vintageMode = !homeStore.vintageMode;
+      console.log(
+        "toggleVintage After, homeStore.vintageMode:" + homeStore.vintageMode
+      );
+      changePage(currentPage, true); // trigger the Page Results component to re-render
+    };
 
-  render() {
-    const { isAuthenticated, user } = this.props.auth;
-    const authFilmLinks = (
-      <Fragment>
-        <GlobalProvider>
-          <FilmProvider>
-            <WatchedProvider>
-              <FilmList />
-              {/*  <Switch>
-                <Route exact path="/"></Route>
-                 popular.results.map((i) => (
-                <Route path={`/movie/${i.id}`} key={i.id}>
-                  <Movie id={i.id} scrollTop={this.scrollTop} />
-                </Route>
-                ))
-              </Switch>*/}
-              <WatchedList />
-            </WatchedProvider>
-          </FilmProvider>
-        </GlobalProvider>
-      </Fragment>
-    );
+    render() {
+      const { isAuthenticated, user } = this.props.auth;
+      // const { changePage, currentPage } = this.props;
 
-    const authLinks = (
-      <Fragment>
-        <NavItem>
-          <span className="navbar-text mr-3">
-            <strong>{user ? `Welcome ${user.name}` : ""}</strong>
-          </span>
-        </NavItem>
-        <NavItem>
-          <Logout />
-        </NavItem>
-        <div className="container"></div>
-      </Fragment>
-    );
+      const authLinks = (
+        <Fragment>
+          <NavItem>
+            <span className="navbar-text mr-3">
+              <strong>{user ? `Welcome ${user.name}` : ""}</strong>
+            </span>
+          </NavItem>
+          <NavItem>
+            <WatchedLink />
+          </NavItem>
+          <NavItem>
+            <VintageLink toggleVintage={this.toggleVintage} />
+          </NavItem>
+          <NavItem>
+            <Logout />
+          </NavItem>
+          <div className="container"></div>
+        </Fragment>
+      );
 
-    const guestLinks = (
-      <Fragment>
-        <NavItem>
-          <RegisterModal />
-        </NavItem>
-        <NavItem>
-          <LoginModal />
-        </NavItem>
-      </Fragment>
-    );
+      const guestLinks = (
+        <Fragment>
+          <NavItem>
+            <RegisterModal />
+          </NavItem>
+          <NavItem>
+            <LoginModal />
+          </NavItem>
+        </Fragment>
+      );
 
-    return (
-      <div
-        style={{
-          backgroundColor: "black",
-        }}
-      >
-        <Navbar color="dark" dark expand="sm" className="mb-5">
-          <Container>
-            <NavbarBrand href="/">
-              <img
-                src={logo}
-                width="138"
-                height="64"
-                className="d-inline-block align-top"
-                alt="Best Rated Films logo"
-              />
-            </NavbarBrand>
-            <NavbarToggler onClick={this.toggle} />
-            <Collapse isOpen={this.state.isOpen} navbar>
-              <Nav className="ml-auto" navbar>
-                {isAuthenticated ? authLinks : guestLinks}
-              </Nav>
-            </Collapse>
-          </Container>
-        </Navbar>
-        <Container>
-          {isAuthenticated ? authFilmLinks : "Login to view your films!"}
-        </Container>
-      </div>
-    );
+      return (
+        <div
+          style={{
+            backgroundColor: "black",
+          }}
+        >
+          <Navbar color="dark" dark expand="sm" className="mb-4">
+            <Container>
+              <NavbarBrand href="/">
+                <img
+                  src={logo}
+                  width="138"
+                  height="64"
+                  className="d-inline-block align-top"
+                  alt="Best Rated Films logo"
+                />
+              </NavbarBrand>
+              <NavbarToggler onClick={this.toggle} />
+              <Collapse isOpen={this.state.isOpen} navbar>
+                <Nav className="ml-auto" navbar>
+                  {isAuthenticated ? authLinks : guestLinks}
+                </Nav>
+              </Collapse>
+            </Container>
+          </Navbar>
+        </div>
+      );
+    }
   }
-}
+);
 
 const mapStateToProps = (state) => ({
   auth: state.auth,
