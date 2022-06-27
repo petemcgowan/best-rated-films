d220d1a3c58f8185dcb17f6a9af7d14dcaa929e9
Pete <peteburquette@gmail.com>
2022-06-27 13:03:42 +0100

    react spring animation + icons

--- client/src/components/FilmDetails2.js
+++ client/src/components/FilmDetails2.js
@@ -9,6 +9,12 @@ import { FilmContext } from "../context/FilmState";
 import { WatchedContext } from "../context/WatchedState";
 import { Link } from "react-router-dom";
 import homeStore from "../store/HomeStore";
+// import { FaBeer } from "react-icons/fa";
+import { IoEyeOutline } from "react-icons/io5";
+
+import { useSpring, animated } from "@react-spring/web";
+import styles from "../styles/styles.module.css";
+
 const nullw500 = require("../images/nullw500.png");
 
 // const dotenv = require("dotenv");
@@ -17,8 +23,22 @@ export const FilmDetails2 = ({ film, scrollTop, changePage }) => {
   const { deleteFilm } = useContext(FilmContext);
   const { addWatched } = useContext(WatchedContext);
   const [isSending, setIsSending] = useState(false);
+  const [animationToggle, setAnimationToggle] = useState(true);
   const isMounted = useRef(true);
   const { pageResults, films, loaded, currentPage, vintageMode } = homeStore;
+
+  const { x } = useSpring({
+    from: { x: 0 },
+    x: animationToggle ? 1 : 0,
+    config: { duration: 1000 },
+  });
+
+  // const handleClick = () => {
+  //   const wrapper = wrapperRef.current;
+  //   wrapper.classList.toggle("is-nav-open");
+  //   console.log("handleClick called");
+  //   sendRequest();
+  // };
 
   // const { scrollTop } = props;
   // set isMounted to false when we unmount the component
@@ -30,8 +50,10 @@ export const FilmDetails2 = ({ film, scrollTop, changePage }) => {
 
   const sendRequest = useCallback(async () => {
     // don't send again while we are sending
+
     if (isSending) return;
     // update state
+    setAnimationToggle(!animationToggle);
     setIsSending(true);
 
     deleteFilm(film._id); // "films to watch" id
@@ -53,30 +75,48 @@ export const FilmDetails2 = ({ film, scrollTop, changePage }) => {
   }, [isSending]); // update the callback if the state changes
 
   return (
-    <div className="movie-item infos-container" key={film.movieDbId}>
-      <Link
-        to={`/movie/${film.movieDbId}`}
-        disabled={isSending}
-        onClick={scrollTop}
-      >
-        <img
-          src={
-            film.poster_path
-              ? `https://image.tmdb.org/t/p/w500${film.poster_path}`
-              : `${nullw500}`
-          }
-          alt={`Movie Poster`}
-        />
-        <div className="infos-box">
-          <div className="infos-one">{film.release_date}</div>
-          <div className="infos-two">{film.title}</div>
-          <div className="infos-three">{film.averageRanking}</div>
+    <div>
+      <div className="movie-item infos-container" key={film.movieDbId}>
+        <Link
+          to={`/movie/${film.movieDbId}`}
+          disabled={isSending}
+          onClick={scrollTop}
+        >
+          <img
+            src={
+              film.poster_path
+                ? `https://image.tmdb.org/t/p/w500${film.poster_path}`
+                : `${nullw500}`
+            }
+            alt={`Movie Poster`}
+          />
+          <div className="infos-box">
+            <div className="infos-one">{film.release_date}</div>
+            <div className="infos-two">{film.title}</div>
+            <div className="infos-three">{film.averageRanking}</div>
+          </div>
+        </Link>
+        <div onClick={sendRequest}>
+          <animated.div
+            className={styles.text}
+            style={{
+              opacity: x.to({ range: [0, 1], output: [0.3, 1] }),
+              textAlign: "center",
+              scale: x.to({
+                range: [0, 0.25, 0.35, 0.45, 0.55, 0.65, 0.75, 1],
+                output: [1, 0.97, 0.9, 1.1, 0.9, 1.1, 1.03, 1],
+              }),
+            }}
+          >
+            <IoEyeOutline
+              className="animate__animated animate__bounce"
+              style={{ color: "purple" }}
+              size="30"
+              type="menu-fold"
+            />
+          </animated.div>
         </div>
-      </Link>
-      <div>
-        <button disabled={isSending} onClick={sendRequest}>
-          Watched
-        </button>
+        {/* IoEyeOutline - add icon here */}
       </div>
     </div>
   );
