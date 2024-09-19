d3dc248f45c5289a5fa695d08a5ff79c908cc8f8
Peter McGowan <peteburquette@gmail.com>
2024-09-19 02:47:14 +0100

    refactoring and watched css

--- client/src/components/FilmDetails2.js
+++ client/src/components/FilmDetails2.js
@@ -9,15 +9,12 @@ import { FilmContext } from "../context/FilmState";
 import { WatchedContext } from "../context/WatchedState";
 import { Link } from "react-router-dom";
 import homeStore from "../store/HomeStore";
-// import { FaBeer } from "react-icons/fa";
 import { IoEyeOutline } from "react-icons/io5";
 
 import { useSpring, animated } from "@react-spring/web";
 import styles from "../styles/styles.module.scss";
 
 const nullw500 = require("../images/nullw500.png");
-
-// const dotenv = require("dotenv");
 
 export const FilmDetails2 = ({ film, scrollTop, changePage }) => {
   const { deleteFilm } = useContext(FilmContext);
@@ -33,7 +30,6 @@ export const FilmDetails2 = ({ film, scrollTop, changePage }) => {
     config: { duration: 1000 },
   });
 
-
   // set isMounted to false when we unmount the component
   useEffect(() => {
     return () => {
@@ -43,7 +39,6 @@ export const FilmDetails2 = ({ film, scrollTop, changePage }) => {
 
   const sendRequest = useCallback(async () => {
     // don't send again while we are sending
-
     if (isSending) return;
     // update state
     setAnimationToggle(!animationToggle);
@@ -61,27 +56,30 @@ export const FilmDetails2 = ({ film, scrollTop, changePage }) => {
 
   return (
     <div>
-      <div className="movie-item infos-container" key={film.movieDbId}>
-        <Link
-          to={`/movie/${film.movieDbId}`}
-          disabled={isSending}
-          onClick={scrollTop}
-        >
-          <img
-            src={
-              film.poster_path
-                ? `https://image.tmdb.org/t/p/w500${film.poster_path}`
-                : `${nullw500}`
-            }
-            alt={`Movie Poster`}
-          />
-          <div className="infos-box">
-            <div className="infos-one">{film.year}</div>
-            <div className="infos-two">{film.title}</div>
-            <div className="infos-three">{film.averageRanking}</div>
-          </div>
-        </Link>
-        <div onClick={sendRequest}>
+      <div className="movie-item" key={film.movieDbId}>
+        <div className="infos-container">
+          <Link
+              to={`/movie/${film.movieDbId}`}
+              disabled={isSending}
+              onClick={scrollTop}
+            >
+              <img
+                src={
+                  film.poster_path
+                    ? `https://image.tmdb.org/t/p/w500${film.poster_path}`
+                    : `${nullw500}`
+                }
+                alt={`Movie Poster`}
+              />
+              <div className="infos-box">
+                <div className="infos-one">{film.year}</div>
+                <div className="infos-two">{film.title}</div>
+                <div className="infos-three">{film.averageRanking}</div>
+              </div>
+            </Link>
+        </div>
+      </div>
+      <div className="watch-button" onClick={sendRequest}>
           <animated.div
             className={styles.text}
             style={{
@@ -102,8 +100,6 @@ export const FilmDetails2 = ({ film, scrollTop, changePage }) => {
             />
           </animated.div>
         </div>
-        {/* IoEyeOutline - add icon here */}
-      </div>
     </div>
   );
 };
